b49dea7df9bdc1a76d8ef9ada1ee0a56363ded37
Jeff Thomas <jeffdthomas@gmail.com>
2021-11-18 10:26:09 -0800

    Add the rest of the generator "tests"

--- tests/src/maze_test.cpp
+++ tests/src/maze_test.cpp
@@ -7,13 +7,56 @@
 
 namespace {
 bool is_perfect_maze(jt::maze::Grid &grid) {
-  return true;
+  // Just checks that every cell is accessible
+  auto d = dijkstra_distances(grid, {0, 0});
+  auto min_distance = *std::min_element(d.begin(), d.end());
+  return min_distance != 0;
 }
-};
+}; // namespace
+
+TEST(mazeTest, TestInaccessibleMaze) {
+  jt::maze::Grid grid{50, 50};
+  EXPECT_FALSE(is_perfect_maze(grid));
+}
 
 TEST(mazeTest, TestBinaryTreeGen) {
-  jt::maze::Grid grid{50,50};
+  jt::maze::Grid grid{50, 50};
   binary_tree_maze(grid);
+
+  EXPECT_TRUE(is_perfect_maze(grid));
+}
+
+TEST(mazeTest, TestSidewinderGen) {
+  jt::maze::Grid grid{50, 50};
+  sidewinder_maze(grid);
+
+  EXPECT_TRUE(is_perfect_maze(grid));
+}
+
+TEST(mazeTest, TestAldousBroderGen) {
+  jt::maze::Grid grid{50, 50};
+  random_walk_Aldous_Broder_maze(grid);
+
+  EXPECT_TRUE(is_perfect_maze(grid));
+}
+
+TEST(mazeTest, TestWilsonGen) {
+  jt::maze::Grid grid{50, 50};
+  random_walk_Wilson_maze(grid);
+
+  EXPECT_TRUE(is_perfect_maze(grid));
+}
+
+TEST(mazeTest, TestHuntAndKillGen) {
+  jt::maze::Grid grid{50, 50};
+  hunt_and_kill_maze(grid);
+
+  EXPECT_TRUE(is_perfect_maze(grid));
+}
+
+TEST(mazeTest, TestRecursiveBacktrackingGen) {
+  jt::maze::Grid grid{50, 50};
+  recursive_backtracking_maze(grid);
 
   EXPECT_TRUE(is_perfect_maze(grid));
 }
